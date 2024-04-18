e2146cb3d69e92af4372716c7121b860d14ddaee
b13nmaN <blenmanjoel@gmail.com>
2024-04-17 21:51:57 -0400

    added the migration

--- database/migrations/2024_04_18_014117_create_movies_table.php
+++ database/migrations/2024_04_18_014117_create_movies_table.php
@@ -10,12 +10,17 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('movies', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('movies', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->text('description')->nullable();
+        $table->string('poster')->nullable();
+        $table->string('path')->nullable();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
