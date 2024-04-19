bc457b41f90db16562bbbceeea7fac802222ebca
b13nmaN <blenmanjoel@gmail.com>
2024-04-19 13:11:39 -0400

    updated MovieView template

--- resources/js/Pages/MovieView.vue
+++ resources/js/Pages/MovieView.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="container">
       <div class="row">
-        <div v-for="movie in movies" :key="movie.id" class="col-md-4">
-          <div class="card mb-3">
-            <img :src="'/storage/posters/' + movie.poster" class="card-img-top" alt="Movie Poster">
-            <div class="card-body">
-              <h5 class="card-title">{{ movie.title }}</h5>
-              <p class="card-text">{{ movie.description }}</p>
+        <div v-for="movie in movies" :key="movie.id" class="card mb-3 me-3 p-0" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img :src="'/storage/posters/' + movie.poster" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">{{ movie.title }}</h5>
+                <p class="card-text">{{ movie.description }}</p>
+              </div>
             </div>
           </div>
         </div>
